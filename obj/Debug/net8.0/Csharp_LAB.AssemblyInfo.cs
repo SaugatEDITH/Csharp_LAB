9b315e32015c71f8c4e2401ef2305d1a3e25fd59
Saugat <saugatpokharel201@gmail.com>
2024-08-07 07:48:17 +0545

    QN One solved

--- obj/Debug/net8.0/Csharp_LAB.AssemblyInfo.cs
+++ obj/Debug/net8.0/Csharp_LAB.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Csharp_LAB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4605f55061b8a2d0d9a4dee93ac9c005349d2fe1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47b972c282c8c0cffa9b15a2e943d09c3957a98a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Csharp_LAB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Csharp_LAB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
