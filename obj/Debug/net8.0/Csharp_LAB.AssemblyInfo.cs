2fae96da560e109e2c8da6ac1ba87bd9c3231d4b
Saugat <saugatpokharel201@gmail.com>
2024-10-30 12:31:31 +0545

    9th qn complete

--- obj/Debug/net8.0/Csharp_LAB.AssemblyInfo.cs
+++ obj/Debug/net8.0/Csharp_LAB.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Csharp_LAB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53304ff041110571b9d68b40ea5c0202e21801da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1bf81d2cc8a994fcbe2010ac768e2d9bf6fc7f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Csharp_LAB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Csharp_LAB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
