2d4463c91714545dbd9dd8c8875c2256db5568a8
Saugat <saugatpokharel201@gmail.com>
2024-10-30 20:20:43 +0545

    16 started

--- 14-15-Stack.cs
+++ 14-15-Stack.cs
@@ -1,5 +1,17 @@
-using System;
-using System.Collections.Generic;
-class Program{
-    
-}
+// using System;
+// using System.Collections.Generic;
+// class Program{
+//     static void Main(string[] args){
+//         Stack<int> stacking=new Stack<int>();
+//         stacking.Push(1);
+//         stacking.Push(2);
+//         stacking.Push(3);
+//         stacking.Push(4);
+// Console.WriteLine("Listing stack items:-");
+// stacking.ToList().ForEach(x => Console.WriteLine(x));
+// Console.WriteLine($"Popping out item from stack {stacking.Pop()}");
+// Console.WriteLine("Listing stack items:-");
+// stacking.ToList().ForEach(x => Console.WriteLine(x));
+
+//     }
+// }
